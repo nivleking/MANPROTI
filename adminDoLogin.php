e11ac353a95ac64c30d657847d9c37ac0c89fd13
Kelvin Sidharta Sie <118882421+nivleking@users.noreply.github.com>
2023-11-01 11:57:46 +0700

    Admin update

--- adminDoLogin.php
+++ adminDoLogin.php
@@ -6,6 +6,7 @@ if(isset($_POST['save'])){
 	$password = $_POST['passwordADM'];
 	$sql = mysqli_query($con,"SELECT * FROM admin where id_admin='$username' and password='$password'");
 	$row  = mysqli_fetch_array($sql); 
+    
     if(is_array($row))
     {
         $_SESSION["usernameADM"]=$row['id_admin'];
@@ -15,10 +16,11 @@ if(isset($_POST['save'])){
     }
     else
     {
-        echo ("<script LANGUAGE='JavaScript'>
-        window.alert('Invalid Username/Password');
-        window.location.href='loginAdmin.php';
-        </script>");
+        header("Location: loginAdmin.php?error=Invalid username or password");
+        // echo ("<script LANGUAGE='JavaScript'>
+        // window.alert('Invalid Username/Password');
+        // window.location.href='loginAdmin.php';
+        // </script>");
     }
 }
 ?>
