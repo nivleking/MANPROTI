3c39b6839d0d7baae93b1349c11740d82844e3ab
Kelvin Sidharta Sie <118882421+nivleking@users.noreply.github.com>
2023-11-01 11:00:35 +0700

    C

--- proyek_manpro.sql
+++ proyek_manpro.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Okt 2023 pada 08.47
--- Versi server: 10.4.25-MariaDB
--- Versi PHP: 8.1.10
+-- Generation Time: Nov 01, 2023 at 04:59 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `admin`
+-- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -35,7 +35,7 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `admin`
+-- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id_admin`, `name_admin`, `password`, `status`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `admin` (`id_admin`, `name_admin`, `password`, `status`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `container`
+-- Table structure for table `container`
 --
 
 CREATE TABLE `container` (
@@ -55,32 +55,139 @@ CREATE TABLE `container` (
   `id_sales` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `container`
+--
+
+INSERT INTO `container` (`id_container`, `asal_container`, `tujuan_container`, `id_sales`) VALUES
+(100, 'SBY', 'MKS', 1),
+(101, 'SBY', 'JKT', 1),
+(102, 'MDR', 'SBY', 2),
+(103, 'SBY', 'MKS', 3),
+(104, 'JKT', 'BPP', 3),
+(105, 'SBY', 'JKT', 3),
+(106, 'SBY', 'BPP', 3),
+(107, 'BPP', 'SBY', 3),
+(108, 'BPP', 'JKT', 5),
+(109, 'JKT', 'SBY', 4),
+(110, 'SUB', 'MDN', 5),
+(111, 'SUB', 'MDN', 5),
+(112, 'SUB', 'MDN', 5),
+(113, 'SUB', 'MDN', 5),
+(114, 'SUB', 'MDN', 5),
+(115, 'SUB', 'MDN', 5),
+(116, 'SUB', 'MDN', 5),
+(117, 'SUB', 'MDN', 5),
+(118, 'SUB', 'MDN', 5),
+(119, 'SUB', 'MDN', 5);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `room`
+-- Table structure for table `room`
 --
 
 CREATE TABLE `room` (
   `id_room` int(4) NOT NULL,
   `id_admin` varchar(4) NOT NULL,
-  `status` int(1) NOT NULL
+  `status` int(1) NOT NULL,
+  `tanggal` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `room`
+-- Dumping data for table `room`
 --
 
-INSERT INTO `room` (`id_room`, `id_admin`, `status`) VALUES
-(13, 'sam1', 0),
-(15, 'sam1', 0),
-(765, 'Akto', 0),
-(999, 'Akto', 0);
+INSERT INTO `room` (`id_room`, `id_admin`, `status`, `tanggal`) VALUES
+(0, 'sam1', 0, '2023-10-30'),
+(13, 'sam1', 0, NULL),
+(15, 'sam1', 0, NULL),
+(115, 'sam1', 1, NULL),
+(123, 'sam1', 0, NULL),
+(124, 'sam1', 0, '2023-10-29'),
+(125, 'sam1', 0, '2023-10-29'),
+(126, 'sam1', 1, '2023-10-29'),
+(127, 'sam1', 1, '2023-10-29'),
+(128, 'sam1', 1, '2023-10-29'),
+(129, 'sam1', 1, '2023-10-29'),
+(130, 'sam1', 1, '2023-10-29'),
+(131, 'sam1', 1, '2023-10-30'),
+(132, 'sam1', 1, '2023-10-30'),
+(133, 'sam1', 1, '2023-10-30'),
+(134, 'sam1', 1, '2023-10-30'),
+(136, 'sam1', 1, '2023-10-30'),
+(157, 'sam1', 0, NULL),
+(174, 'sam1', 1, '2023-10-30'),
+(178, 'sam1', 0, NULL),
+(179, 'sam1', 0, NULL),
+(180, 'sam1', 0, '2023-10-29'),
+(181, 'sam1', 1, '2023-10-29'),
+(317, 'sam1', 1, NULL),
+(431, 'sam1', 0, NULL),
+(456, 'sam1', 1, NULL),
+(500, 'sam1', 0, NULL),
+(501, 'sam1', 0, NULL),
+(600, 'sam1', 0, NULL),
+(601, 'sam1', 1, NULL),
+(602, 'sam1', 0, NULL),
+(603, 'sam1', 1, NULL),
+(765, 'Akto', 0, NULL),
+(999, 'Akto', 0, NULL),
+(1111, 'sam1', 0, NULL),
+(11111, 'sam1', 1, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `id_sales` int(255) NOT NULL,
+  `priority` text NOT NULL,
+  `origin` text NOT NULL,
+  `destination` text NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `revenue` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`id_sales`, `priority`, `origin`, `destination`, `quantity`, `revenue`) VALUES
+(5, 'COMMIT', 'SUB', 'MDN', 10, 10000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp_container`
+--
+
+CREATE TABLE `temp_container` (
+  `id_container` int(11) NOT NULL,
+  `id_user` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `temp_container`
+--
+
+INSERT INTO `temp_container` (`id_container`, `id_user`) VALUES
+(110, 'Vincentius'),
+(111, 'Vincentius'),
+(112, 'Vincen'),
+(112, 'Vincentius'),
+(113, 'Vincentius'),
+(115, 'Vincentius'),
+(103, 'Vincentius'),
+(114, 'Vincentius'),
+(100, 'Vincentius');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -92,50 +199,56 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`team_name`, `password`, `ship`, `status`, `id_room`) VALUES
-('AktoBabi', '12345', '[[[0,\"13\",3],[4,1,6],[7,8,9]],[[0,0,0],[\"98\",0,0],[\"23\",0,0]],[[0,0,0],[0,0,0],[0,0,0]]]', 1, 765),
-('AktoKontol', '12345', '[[0,1,2],[3,4,5],[6,7,8]]', 1, 765);
+('Aktonoi', '12345', '[[[\"109\",0,0],[\"100\",\"101\",\"107\"],[\"102\",\"105\",\"103\"]],[[0,0,0],[\"104\",0,0],[\"106\",0,0]],[[0,0,0],[0,0,0],[0,0,0]]]', 1, 132),
+('Vincentius', '12345', '[[[\"107\",0,0],[\"109\",\"101\",0],[\"102\",\"105\",0]],[[0,0,0],[\"104\",0,0],[\"106\",0,0]],[[0,0,0],[0,0,0],[0,0,0]]]', 1, 174);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `admin`
+-- Indexes for table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
--- Indeks untuk tabel `container`
+-- Indexes for table `container`
 --
 ALTER TABLE `container`
   ADD PRIMARY KEY (`id_container`);
 
 --
--- Indeks untuk tabel `room`
+-- Indexes for table `room`
 --
 ALTER TABLE `room`
   ADD PRIMARY KEY (`id_room`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id_sales`);
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`team_name`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `container`
+-- AUTO_INCREMENT for table `container`
 --
 ALTER TABLE `container`
-  MODIFY `id_container` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_container` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
